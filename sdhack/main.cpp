dcc3d02d4bc573a51d2841f91a5c9d129610702d
lboymoushakian <laritalar1997@gmail.com>
2016-10-01 01:21:27 -0700

    first commit

--- sdhack/main.cpp
+++ sdhack/main.cpp
@@ -1,15 +1,59 @@
-//
-//  main.cpp
-//  sdhack
-//
-//  Created by Lari Boymoushakian on 10/1/16.
-//  Copyright © 2016 Lari Boymoushakian. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    
+    string rArray[460];
+    string pArray[90];
+    
+    //feeds list of responses into an array
+    ifstream file("rwords.txt");
+    if(file.is_open())
+    {
+        
+        
+        for(int i = 0; i != 460; i++)
+        {
+            file >> rArray[i];
+        }
+    }
+    
+    //feeds list of prompts into an array
+    ifstream file2("bwords.txt");
+    if(file2.is_open())
+    {
+        
+        for(int i = 0; i != 90; i++)
+        {
+            file2 >> pArray[i];
+        }
+    }
+    
+    //initializes random number generator
+    srand((unsigned) time(NULL));
+    
+    
+    string sInput = "";
+    string sResponse = "";
+    
+    while(1) {
+        cout << ">";
+        getline(cin, sInput);
+        int nSelection = rand() % 90;
+        sResponse = pArray[nSelection];
+        nSelection = rand() % 460;
+        sResponse += rArray[nSelection];
+        cout << sResponse << endl;
+    }
+    
     return 0;
 }
+
+
+
+
+
