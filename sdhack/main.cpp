e37eb524956a57b7bdfef1e1963be160e3890e3a
lboymoushakian <laritalar1997@gmail.com>
2016-10-01 01:16:50 -0700

    Initial Commit

---
+++ sdhack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sdhack
+//
+//  Created by Lari Boymoushakian on 10/1/16.
+//  Copyright © 2016 Lari Boymoushakian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
